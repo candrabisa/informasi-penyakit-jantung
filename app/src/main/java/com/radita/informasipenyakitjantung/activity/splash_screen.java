28f5692f81aa400c68d0663bb693e4df5e15dedb
candraseng <candrabillysagita@gmail.com>
2020-07-17 16:51:20 +0700

    finish

---
+++ app/src/main/java/com/radita/informasipenyakitjantung/activity/splash_screen.java
@@ -0,0 +1,26 @@
+package com.radita.informasipenyakitjantung.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.radita.informasipenyakitjantung.R;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splash_screen.this, MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
